cb78e18d0ef834a95bf479f2c65f1567bb95fcbc
ClemensFMN <clemens@familie-novak.net>
2015-05-19 11:02:02 +0200

    restructured code, made class constructor private so that companion object is the only entry point for class creation

--- fp-in-scala/ClassObject.scala
+++ fp-in-scala/ClassObject.scala
@@ -9,7 +9,9 @@ object OTst {
 	def objectFunction() = println("Hello World")
 }
 
-class D(n: Int) {
+// by declaring the constructor private, val d1 = new D(4) is not possible
+// anymore -> object creation is possible via the companion object only!
+class D private(n: Int) {
 	private var n_int = n
 	def increment() = {n_int = n_int + 1}
 	override def toString() = "Value = " + n_int
@@ -19,4 +21,27 @@ object D {
 	// having a companion object with an apply method, allows for val d = D(4)
 	def apply(n: Int):D = new D(n)
 }
+
+
+object ClassObject {
+	def runit() = {
+		val c1 = new C(3)
+        println(c1)
+
+        OTst.objectFunction
+
+		/*
+        val d1 = new D(4)
+        println(d1)
+        d1.increment()
+        println(d1)
+		*/
+        // this is made possible via the companion object...
+        val d2 = D(4)
+        println(d2)
+        d2.increment()
+        println(d2)
+	}
+}
+
 
--- fp-in-scala/app.scala
+++ fp-in-scala/app.scala
@@ -8,19 +8,6 @@ object Hi {
         //CaseClasses.runit
         //Chap03.runit()
 
-        val c1 = new C(3)
-        println(c1)
-
-        OTst.objectFunction
-
-        val d1 = new D(4)
-        println(d1)
-        d1.increment()
-        println(d1)
-
-        val d2 = D(4)
-        println(d2)
-        d2.increment()
-        println(d2)
+        ClassObject.runit()
     }
 } 
