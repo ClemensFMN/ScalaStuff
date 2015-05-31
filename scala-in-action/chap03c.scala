f63fb556bace411a5af63a46be72e045302af4de
Clemens Novak <clemens@familie-novak.net>
2015-05-31 10:45:28 +0200

    Fibonacci with memoization

--- scala-in-action/chap03c.scala
+++ scala-in-action/chap03c.scala
@@ -1,19 +1,25 @@
 object chap03c {
 	
+	// this is the map which holds already calculated stirling numbers
 	val history = scala.collection.mutable.Map[(Int, Int), Int]()
-	var calls: Int = 0
+	// debug variable to count the number of calls
+	//var calls: Int = 0
 
+	// the public interface method
 	def StirlingSecond(n: Int, k: Int): Int = {
 
+		// perform a look-up for the (n,k) pair; if not in the map, start calculating
 		history.getOrElseUpdate((n,k), StirlingSecondInt(n, k))
 		//StirlingSecondInt(n, k)
 
 	}
 
-
+	// the function doing the real work
 	def StirlingSecondInt(n: Int, k: Int): Int = {
 		//println("Called with:", n, k)
-		calls = calls + 1
+		//calls = calls + 1
+		// based on a pattern match, calculate the stirling numbers according
+		// to the "textbook definition"
 		(n,k) match {
 			case (0,0) => 1
 			case (n,1) => 1
@@ -22,6 +28,41 @@ object chap03c {
 			case _ => error("not implemented")
 		}
 	}
+
+
+	def FibonacciV1(n: Int): Int = {
+		n match {
+			case 1 => 1
+			case 2 => 1
+			case _ => FibonacciV1(n-1) + FibonacciV1(n-2)
+		}
+	}
+
+
+	def FibonacciV2(n: Int) = {
+
+		val historyFib = scala.collection.mutable.Map[Int, Int]()
+		var calls: Int = 0
+
+		def FibonacciCalc(n: Int): Int = {
+			calls = calls + 1
+			n match {
+				case 1 => 1
+				case 2 => 1
+				case _ => FibonacciInt(n-1) + FibonacciInt(n-2)
+			}
+		}
+
+		def FibonacciInt(n: Int): Int = {
+				historyFib.getOrElseUpdate(n, FibonacciCalc(n))
+				//FibonacciCalc(n)
+		}
+	
+	(FibonacciInt(n), calls, historyFib)
+
+	}
+
+
 
 
 	def runit() = {
@@ -37,9 +78,15 @@ object chap03c {
 
 		println(history)
 */
-		println(StirlingSecond(10,5))
-		println("Number of calls: " + calls)
+		//println(StirlingSecond(10,5))
+		
 
+		for(i <- 1 to 10) {
+			println(FibonacciV1(i))
+
+			var x = FibonacciV2(i)
+			println("Argument: " + i + " => Result: " + x)
+		}
 	}
 
 }
