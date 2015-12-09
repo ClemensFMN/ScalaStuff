f187f7c9e8862184eb5e8e15390cd06f3a1c1003
Clemens Novak <clemens@familie-novak.net>
2015-12-09 20:31:54 +0100

    updated testing

--- algorithms/src/test/scala/alg/test-1.scala
+++ algorithms/src/test/scala/alg/test-1.scala
@@ -1,22 +1,25 @@
 package alg
 
 import org.scalacheck._
-import org.scalacheck._
+import org.scalacheck.Prop._
+import org.scalacheck.Gen._
+
 
 object Test1 extends Properties("QSort") {
 
-	property("sorting, v1") = Prop.forAll {(lst: List[Int]) => lst.sorted == Qsort.qsort_v1(lst)}
-	property("sorting, v2") = Prop.forAll {(lst: List[Int]) => lst.sorted == Qsort.qsort_v2(lst)}
+	property("sorting, v1") = forAll {(lst: List[Int]) => lst.sorted == Qsort.qsort_v1(lst)}
+	property("sorting, v2") = forAll {(lst: List[Int]) => lst.sorted == Qsort.qsort_v2(lst)}
 }
 
 
 object Test2 extends Properties("Palindrome") {
 
-	// tricky to test, as 120 becomes 21
-	val smallInteger = Gen.choose(11,19)
-	property("palindrome, 1") = Prop.forAll (smallInteger)(x=> x == Palindrome.reverse_int(Palindrome.reverse_int(x)))
+	val smallInteger = Gen.choose(0,10000)
 
-	val smallInteger2 = Gen.choose(0,10000)
-	property("palindrome, 2") = Prop.forAll(smallInteger2)(x => Palindrome.reverse_int_to_vec_int(x, List()).last == x%10)
+	property("reverse reverse") = forAll(smallInteger){x:Int => 
+			(x % 10 != 0) ==> 
+			(x == Palindrome.reverse_int(Palindrome.reverse_int(x)))}
+
+	property("first element") = forAll(smallInteger)(x => Palindrome.reverse_int_to_vec_int(x, List()).last == x%10)
 }
 
