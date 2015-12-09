c02159ad118c3440470527cec468d9e6b7707dd5
ClemensFMN <clemens@familie-novak.net>
2015-12-09 12:37:10 +0100

    reorganized dir structure, tests cannot find classes

--- algorithms/App.scala
+++ algorithms/App.scala
@@ -4,5 +4,8 @@ object App {
 
 		Qsort.runit()
 
+		//val A = List(3, -5, -10, 1, 3, 3, -1)
+		//println(Qsort.qsort_v1(A))
+
 	}
 }
--- algorithms/src/test/scala/simple.scala
+++ algorithms/src/test/scala/simple.scala
@@ -3,9 +3,7 @@ import org.scalacheck.Prop.forAll
 
 object QSort extends Properties("QSort") {
 
-	//property("sorting, v1") = forAll {(lst: List[Int]) => lst.sorted == App.qsort_v1(lst)}
-	property("sorting, v2") = forAll {(lst: List[Int]) => lst.sorted == App.qsort_v2(lst)}
-
-
+	property("sorting, v1") = forAll {(lst: List[Int]) => lst.sorted == Qsort.qsort_v1(lst)}
+	property("sorting, v2") = forAll {(lst: List[Int]) => lst.sorted == Qsort.qsort_v2(lst)}
 }
 
