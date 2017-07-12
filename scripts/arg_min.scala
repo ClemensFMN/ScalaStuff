3934e0703dcefe99e1a97e83d775f2772e27d97b
Clemens Novak <clemens@familie-novak.net>
2017-07-12 14:17:19 +0200

    added argmin from scripts

---
+++ scripts/arg_min.scala
@@ -0,0 +1,19 @@
+// given a list of (random) numbers, find the position and value of the minimum value
+
+//list length
+val N = 5
+
+// create a list with random elements; from http://alvinalexander.com/scala/how-to-generate-random-numbers-characters-sequences-in-scala
+val rng = scala.util.Random
+val data = for(i<- 0 to N) yield rng.nextInt(10)
+
+// zip the data together with an index -> this yields a list of tuples (value,index)
+val full = data.zip(Range(0,N))
+
+full.foreach(println)
+
+// search the list minimum based on first element in the tuples
+val min = full.minBy(e => e._1)
+
+println("Minimum value is: " + min)
+
