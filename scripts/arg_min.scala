8f868ff170244dafbb766d5a9c2acfe2c2122baa
Clemens Novak <clemens@familie-novak.net>
2017-07-12 14:22:39 +0200

    made a bit nicer

--- scripts/arg_min.scala
+++ scripts/arg_min.scala
@@ -8,12 +8,14 @@ val rng = scala.util.Random
 val data = for(i<- 0 to N) yield rng.nextInt(10)
 
 // zip the data together with an index -> this yields a list of tuples (value,index)
-val full = data.zip(Range(0,N))
+// val full = data.zip(Range(0,N))
+// somewhat nicer in that the length of the array is not needed for the zip...
+val full = data.zipWithIndex
 
 full.foreach(println)
 
 // search the list minimum based on first element in the tuples
 val min = full.minBy(e => e._1)
 
-println("Minimum value is: " + min)
+println("Minimum value is: " + min._1 + ", located at: " + min._2)
 
