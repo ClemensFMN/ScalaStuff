531734e9956695391cd4bf896c2d3ff277be6d82
Clemens Novak <clemens@familie-novak.net>
2018-02-12 13:22:02 +0100

    extended chap 2

--- fp-in-scala/Chap02.scala
+++ fp-in-scala/Chap02.scala
@@ -19,12 +19,35 @@ object Chap02 {
         x => x + p
     }
 
+    def findFirst[A](as:Array[A], p: A => Boolean) : Int = {
+        def loop(n: Int): Int = {
+            if(n >= as.length) -1
+            else if (p(as(n))) n
+            else loop(n+1)
+        }
+        loop(0)
+    }
+
+    def myPartial[A,B,C](a:A, f:(A,B) => C): B => C = {
+        (b:B) => f(a,b) // return a function which takes a parameter b of type B and returns the value f(a,b)
+    }
+
+    def myCurry[A,B,C](f:(A,B)=>C):A=>(B=>C) = {
+        a => (b => f(a,b))
+    }
+
+    def myUncurry[A,B,C](f:A=>(B=>C)):(A,B)=>C = {
+        (a:A, b:B) => f(a)(b)
+    }
+
 
     def runit() = {
-    	val f = (x:Int)=>2 * x
-        val g = (x:Int)=>x + 1
-        val fg = compose(g,f)
+    	val f = (x:Int)=> 2*x
+        val g = (x:Int)=> x+1
+        val fg = compose(g,f) // this yields 2*x+1
         println(fg(4))
+        val gf = compose(f,g) // this yields 2*(x+1)
+        println(gf(4))
 
         val l = List(1,2,3)
         println(myFold(l, (x:Int,acc:Int) => x + acc, 0))
@@ -33,6 +56,17 @@ object Chap02 {
         val add3 = parametricAdder(3)
         // and use it
         println(add3(7))
+
+        println(findFirst(Array(1,2,3,4), (x:Int)=>x==2))
+        println(findFirst(Array(1,2,3,4), (x:Int)=>x>2))
+
+        val f1 = (x:Int, y:Int) => x+y
+        val fpartial = myPartial(2,f1)
+        println(fpartial(3))
+
+        val fcurry = myCurry(f1)
+        val funcurry = myUncurry(fcurry)
+        println(f1(2,3), fcurry(2)(3), funcurry(2,3))
 
     }
 }
--- fp-in-scala/app.scala
+++ fp-in-scala/app.scala
@@ -4,10 +4,10 @@ object Hi {
     def main(args: Array[String]) {
 
         //ListStuff.runit
-        //Chap02.runit
-        CaseClasses.runit
+        Chap02.runit
+        //CaseClasses.runit
         //Chap03.runit()
-	//Chap04.runit()
+	      //Chap04.runit()
         //ClassObject.runit()
         //Chap06.runit()
     }
